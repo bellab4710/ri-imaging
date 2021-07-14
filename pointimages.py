6f68367c461df58864c0c771a338ff575927ae75
Isabella Bryant <84340108+bellab4710@users.noreply.github.com>
2021-07-14 12:23:01 -0700

    Update pointimages.py

--- pointimages.py
+++ pointimages.py
@@ -1,9 +1,9 @@
 import numpy as np
-from scipy.misc import toimage
+from scipy.misc import imshow
 rng = np.random.default_rng()
 xrints = rng.integers(low=0,high=127, size = 5)
 yrints = rng.integers(low=0,high=127, size = 5)
 baseimage = np.zeroes((128,128))
 for i in range(5):
     baseimage[x[i]][y[i]] = 1
-toimage(baseimage).show()
+imshow(baseimage)
