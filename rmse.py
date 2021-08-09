395c073557013580e44583fbcbe2ce61137d558d
Isabella Bryant <84340108+bellab4710@users.noreply.github.com>
2021-08-09 09:59:13 -0700

    Create rmse.py

---
+++ rmse.py
@@ -0,0 +1,22 @@
+import numpy as np
+from PIL import Image
+
+def rmse(observed,expected):
+  return np.sqrt(np.sum((observed - expected)**2 / len(observed)))
+
+def main():
+  file = open('rmse.txt','w')
+  for i in range(10):
+    img1 = np.loadtxt('./scat'+str(i+1)+'.txt')
+    img2 = np.loadtxt('./blurScat'+str(i+1)+'.txt')
+    imvec1 = img1.flatten()
+    imvec2 = img2.flatten()
+    val = rmse(imvec1, imvec2)
+    file.write("RMSE for Image"+str(i+1)+": "+str(val)+"\n")
+  file.close()
+ 
+if __name__ == '__main__':
+    main()
+    
+    
+                    
